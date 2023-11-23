2bb6a424ba16bf93ea9fdd5672d0f9298f526574
imran <alimrancv870@gmail.com>
2023-11-23 15:07:27 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { React } from 'react'
-import Header from './components/Header';
+import Header from './components/header';
 import Banner from './components/banner';
 import Aplications from './components/Aplications';
 import Wallet from './components/Wallet';
