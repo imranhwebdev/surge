6bce9210e695ec6f07b8377c1bdc556a6e6008b4
imran <alimrancv870@gmail.com>
2023-11-24 12:25:23 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Banner from './components/banner';
 import Aplications from './components/Aplications';
 import Wallet from './components/Wallet';
 import Trade from './components/Trade';
+import Snipe from './components/Snipe';
 import Footer from './components/Footer';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Aplications />
       <Wallet />
       <Trade />
+      <Snipe />
       <Footer />
     </>
   )
