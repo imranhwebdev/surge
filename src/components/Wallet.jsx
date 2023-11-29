51eedca47ea45a49b7a1b0893613e896bcfa108d
imran <alimrancv870@gmail.com>
2023-11-30 00:21:24 +0600

    update

--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -1,12 +1,50 @@
-import {React} from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { Col, Container, Row, Tab, Nav } from 'react-bootstrap'
 import SectionTitle from './SectionTitle'
-import WalletRightImg from '../assets/img/trading_wallet_right-img.png'
+import WalletRightImg from '../assets/img/trading_wallet_right-img.png';
 export default function Wallet(){
     const Title = 'Security';
     const Desc ='Your Assets, Your Control. We never store your private keys.';
     const BtnText ='Read More';
     const Btnlink ='https://google.com';
+    const Title2 = 'Self-Sovereignty';
+    const Desc2 ='You decide - automated trading or manual control.';
+    const BtnText2 ='Read More';
+    const Btnlink2 ='https://google.com';
+    const Title3 = 'Automation';
+    const Desc3 ='Trade smarter with customizable trading strategies.';
+    const BtnText3 ='Read More';
+    const Btnlink3 ='https://google.com';
+    const Title4 = 'Efficiency';
+    const Desc4 ='Seamless and swift execution of your trades.';
+    const BtnText4 ='Read More';
+    const Btnlink4 ='https://google.com';
+
+
+    const [rotationAngle, setRotationAngle] = useState('0deg');
+
+    const handleTabClick = (tabKey) => {
+        // Set the rotation angle based on the active tab
+        let newRotationAngle = '0deg';
+
+        switch (tabKey) {
+            case 'second':
+                newRotationAngle = '90deg';
+                break;
+            case 'third':
+                newRotationAngle = '180deg';
+                break;
+            case 'four':
+                newRotationAngle = '360deg';
+                break;
+            // Add more cases for additional tabs if needed
+
+            default:
+                break;
+        }
+
+        setRotationAngle(newRotationAngle);
+    };
     return(
         <div className="wallet__area" id='wallet'>
             <Container>
@@ -16,22 +54,67 @@ export default function Wallet(){
                 />
             </Container>
             <Container>
-                <Row className='align-items-center'>
-                   <Col md='6'>
-                        <div className="wallet_single_box">
-                            <h3>{Title}</h3>
-                            <p>{Desc}</p>
-                            <a target='_blank' href={Btnlink} className='inline__btn'>{BtnText}</a>
-                        </div>
-                   </Col>
-                   <Col md='6'>
-                        <figure className='w_i'>
-                            <img src={WalletRightImg} alt="Wallet Image" />
-                        </figure>
-                   </Col>
-                </Row>
+                <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+                    <Row className='align-items-center'>
+                        <Col md='6'>
+                            <Tab.Content>
+                                <Tab.Pane eventKey="first">
+                                    <div className="wallet_single_box">
+                                        <h3>{Title}</h3>
+                                        <p>{Desc}</p>
+                                        <a target='_blank' href={Btnlink} className='inline__btn'>{BtnText}</a>
+                                    </div>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="second">
+                                    <div className="wallet_single_box">
+                                        <h3>{Title2}</h3>
+                                        <p>{Desc2}</p>
+                                        <a target='_blank' href={Btnlink2} className='inline__btn'>{BtnText2}</a>
+                                    </div>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="third">
+                                    <div className="wallet_single_box">
+                                        <h3>{Title3}</h3>
+                                        <p>{Desc3}</p>
+                                        <a target='_blank' href={Btnlink3} className='inline__btn'>{BtnText3}</a>
+                                    </div>
+                                </Tab.Pane>
+                                <Tab.Pane eventKey="four">
+                                    <div className="wallet_single_box">
+                                        <h3>{Title4}</h3>
+                                        <p>{Desc4}</p>
+                                        <a target='_blank' href={Btnlink4} className='inline__btn'>{BtnText4}</a>
+                                    </div>
+                                </Tab.Pane>
+                            </Tab.Content> 
+                        </Col>
+                        <Col md='6'>
+                            <figure className='w_i'>
+                                <img
+                                    src={WalletRightImg}
+                                    alt="Wallet Image"
+                                    style={{ transform: `rotate(${rotationAngle})`, transition: 'transform 0.5s ease-in-out' }}
+                                />
+                                <div className="all_click_icons">
+                                    <Nav.Link eventKey="first" className='sc' onClick={() => handleTabClick('first')}>
+                                        sc
+                                    </Nav.Link>
+                                    <Nav.Link eventKey="second" className='ss' onClick={() => handleTabClick('second')}>
+                                        ss
+                                    </Nav.Link>
+                                    <Nav.Link eventKey="third" className='au' onClick={() => handleTabClick('third')}>
+                                        au
+                                    </Nav.Link>
+                                    <Nav.Link eventKey="four" className='eff' onClick={() => handleTabClick('four')}>
+                                        eff
+                                    </Nav.Link>
+                                </div>
+                            </figure>
+                        </Col>
+                    </Row>
+                </Tab.Container>
             </Container>
            
         </div>
-    )
+    );
 }
